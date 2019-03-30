9aba31baf2fcf21435e71fa03ca99eb6c5140913
Paul Grevink <paul.grevink@gmail.com>
2019-03-30 15:32:14 +0100

    Update multidimensionalarrays.ps1
    Multi-dimensional array in a loop was wrong.

--- multidimensionalarrays.ps1
+++ multidimensionalarrays.ps1
@@ -1,22 +1,39 @@
-Source: https://devblogs.microsoft.com/scripting/easily-create-and-manipulate-an-array-of-arrays-in-powershell/
+# Source: https://devblogs.microsoft.com/scripting/easily-create-and-manipulate-an-array-of-arrays-in-powershell/
 
 $a = Get-Service
 $b = Get-Process
 $c = Get-Date
 
 # One dimensrional array
-$array = @()
-$array += $a
-$array += $b
-$array += $c
-Write-Host $array.count
+Write-Host "One dimensional array"
+$array1 = @()
+Write-Host $array1.count
+$array1 += $a
+Write-Host $array1.count
+$array1 += $b
+Write-Host $array1.count
+$array1 += $c
+Write-Host $array1.count
 
 # multi-dimensional array
+Write-Host "Multi dimensional array"
+$array2 = @()
+Write-Host $array2.count
 $array2 = $a,$b,$c
 Write-Host $array2.count
 
 # Multi-dimensional which can be used in a loop
-$array3 = $a
-# Next line can be used in a loop
-$array3 = $array3,$c
+# https://stackoverflow.com/questions/6157179/append-an-array-to-an-array-of-arrays-in-powershell
+
+Write-Host "Multi dimensional array used in a loop"
+[Array]$array3 = @()
 Write-Host $array3.count
+$array3 += , $a
+Write-Host $array3.count
+Write-Host $array3[0]
+$array3 += , $b
+Write-Host $array3.count
+$array3 += , $c
+Write-Host $array3.count
+Write-Host $array3[2]
+
